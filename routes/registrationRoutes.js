53ec89e470d42486df71eb375948d97c08b87654
nabanitabania <nabanitabania78@gmail.com>
2020-01-11 15:34:50 +0530

    AB

--- routes/registrationRoutes.js
+++ routes/registrationRoutes.js
@@ -11,20 +11,7 @@ var html = fs.readFileSync(__dirname+'/template.html', 'utf8');
 
 var options = {
         format: "A3",
-        orientation: "portrait",
-        border: "10mm",
-        header: {
-            height: "45mm",
-            contents: '<div style="text-align: center;">Author: Shyam Hajare</div>'
-        },
-        "footer": {
-            "height": "28mm",
-            "contents": {
-            first: 'Cover page',
-            2: 'Second page', // Any page number is working. 1-based index
-            default: '<span style="color: #444;">{{page}}</span>/<span>{{pages}}</span>', // fallback value
-            last: 'Last Page'}}
-        
+        orientation: "portrait",      
     };
 
 
